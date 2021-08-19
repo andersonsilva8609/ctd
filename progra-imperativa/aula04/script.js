36f73e1430a7c7defa7dcf802c49013f0a6a0238
Anderson da Silva <andersonsilva8609@gmail.com>
2021-08-19 00:39:04 -0300

    update  trabalho de mesa aula05

--- progra-imperativa/aula04/script.js
+++ progra-imperativa/aula04/script.js
@@ -1,7 +1,18 @@
+function conversao() {
 var saudacao = 'Olá';
 var nome = prompt('Qual é o seu nome ?');
 var dolares = 0;
 var reais = 0;
+var confimacao = 0;
 dolares = prompt('Quantos dolares você gostaria de cotar ? ');
+confimacao = prompt('Confirma o valor de dolares você gostaria de cotar ? ');
+confimacao === dolares ? reais : digiteNovamente;
+var digiteNovamente = prompt('Digite o valor novamente');
 reais = dolares * 5.3;
 alert(saudacao +' '+nome +' US$'+' '+dolares+' vale'+' R$ '+reais+' hoje.');
+
+setTimeout(function(){
+  window.location.href='https://www.cotacao.com.br/';
+}, 5000);
+}
+conversao();
---
+++ progra-imperativa/aula05/script.js
@@ -0,0 +1,35 @@
+/*
+Matemáticos
++ mais
+- menos
+* multiplicação
+/ divisão
+% resto de divisão
+** potência
+
+Lógicos - responder verdadeiro(true) ou falso(false)
+< maior
+> menor
+<= menor ou igual
+>= maior ou igual
+== é igual?
+=== é igual mesmo ?
+!= é diferente de ?
+
+Condicionais(ternários)
+[condição]?[caso seja verdadeiro]:[caso seja falso]
+exemplo: 4 > 6 ? 'É maior' : 'É menor'
+
+*/
+
+//1.Função com parametro
+//Um objetivo
+function saudar(mensagem, nomeDoUsuario){
+  alert(mensagem + ' ' + nomeDoUsuario);
+}
+
+//2. Métodos
+saudar(1235);
+saudar('Olá');
+saudar('n');
+saudar('');
