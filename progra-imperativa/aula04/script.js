37d8ed38d5c2b9c6e148c977afd2a25a3ed3c01c
Anderson da Silva <andersonsilva8609@gmail.com>
2021-08-19 20:51:23 -0300

    atidade refaturação da aula05 e código da aula06

--- progra-imperativa/aula04/script.js
+++ progra-imperativa/aula04/script.js
@@ -6,13 +6,13 @@ var reais = 0;
 var confimacao = 0;
 dolares = prompt('Quantos dolares você gostaria de cotar ? ');
 confimacao = prompt('Confirma o valor de dolares você gostaria de cotar ? ');
-confimacao === dolares ? reais : digiteNovamente;
-var digiteNovamente = prompt('Digite o valor novamente');
-reais = dolares * 5.3;
+dolares === confimacao ? reais : digiteNovamente;
+reais = dolares * 5.61;
+digiteNovamente = prompt('Digite o valor novamente');
 alert(saudacao +' '+nome +' US$'+' '+dolares+' vale'+' R$ '+reais+' hoje.');
 
 setTimeout(function(){
-  window.location.href='https://www.cotacao.com.br/';
+  window.open ='https://www.cotacao.com.br/';
 }, 5000);
 }
 conversao();
