2061f1b4ac0092a6f2c5bc3b2f656d4b1b1f1582
Anderson da Silva <andersonsilva8609@gmail.com>
2021-09-01 22:55:34 -0300

    Front-end aula12

---
+++ programacao-imperativa/aula11/script.js
@@ -0,0 +1,13 @@
+let lista = ["Feijão", "Sabonete", "Arroz", "Escova", "Farinha"]
+ 
+console.log(lista[0].split('i'));
+// console.log("O método Join junta índices de um array, podendo colocar um caractere entre cada índice.Ex: " + lista.join(" - "))
+// console.log("O método Pop remove e retorna o último índice do array. Ex: " + lista.pop())
+ 
+// lista.push("Final")
+// console.log("O método Push adiciona um novo valor ao array, no seu final. Ex: " + lista)
+ 
+// console.log("O método Shift remove e retorna o primeiro elemento do array. Ex: " + lista.shift())
+ 
+// lista.unshift("Começo")
+// console.log("O método Push adiciona um novo valor ao array, no seu começo. Ex: " + lista)
