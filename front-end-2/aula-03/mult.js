00fa6a7ab0d6f11efcb33fe403228e46e1e65d6a
Anderson Silva <35344735+andersonsilva8609@users.noreply.github.com>
2022-03-16 20:49:00 -0300

    delete

--- front-end-2/aula-03/mult.js
+++
@@ -1,6 +0,0 @@
-export default function multi(a, b){
-    if(a = 0){
-        console.log(' Ops! Insira um número deferente de ZERO. ');
-    }
-    return a * b ;
-}
--- front-end-2/aula-05/main.js
+++
@@ -1,8 +0,0 @@
-let mainMenuReference = document.querySelector('#mainMenu');
-
-function manipuladorMenu(){
-
-     //verifica se a classe existe, add ou remove
-    mainMenuReference.classList.toggle('main-menu-opened');
-
-}
